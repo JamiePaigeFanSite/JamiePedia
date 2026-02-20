d76bf4d1e564db848b8980d96933d0fa5e10ae3d
Xtra3678 <113572269+Xtra3678@users.noreply.github.com>
2026-02-20 01:04:21 -0500

    Delete assets directory

--- assets/static/load.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  // Load local include fragments (relative paths) to avoid cross-origin issues
-  $("#navi").load("assets/static/navibar.html");
-  $("#sidebar").load("assets/static/sidebar.html");
-  $("#linkbox").load("assets/static/linkbox.html");
-});
